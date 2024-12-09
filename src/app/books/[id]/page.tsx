da3d9766bd389328c3f1ca8642d3b315416d6ba1
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-12-09 17:15:52 +0900

    초기 세팅

---
+++ src/app/books/[id]/page.tsx
@@ -0,0 +1,13 @@
+export default function BookDetail() {
+  //   useEffect(() => {
+  // // 데이터 가져오기
+  //   });
+
+  //   if (!data) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>id.book 페이지</h1>
+    </div>
+  );
+}
---
+++ src/app/books/add/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { useState } from "react";
+
+export default function AddBook() {
+  return <h1>add page</h1>;
+}
